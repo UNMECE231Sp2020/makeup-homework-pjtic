5a34bb2efaf095b99c8ff855162190f3ec26c05a
pjtic <pauly4iron@gmail.com>
2020-04-20 20:42:23 -0600

    debuggin

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -7,54 +7,13 @@ void swap(int *x, int *y){
 }
 
 void bubbleSort(int *begin, const int *end){
-/*	
-	 for( int i=1; i>0;++i){
-	  int temp = begin[i];
-	  int j;
-	  
-	  	for(j=i-1; j>=0 && begin[i]>temp;--j) {
-			begin[j+1] = begin[j];
-		}
-		begin[j+1] = temp;
-	 } */	
+	
+	
+		int *headTemp, *nextTemp;
 
+	for(headTemp = begin; headTemp!=end; ++headTemp){
 	
-	/*
-	 *
-	int i, j;
-	for(i=0;i<n-1;++i){
-	
-	for(j=0;j<n-i-1;++j){
-		if(array[j]>array[j+1]){
-			swap(&array[j],&array[j+1]);
-		}
-	}
-	}
-}
-	
-*/
-
-	
-	       
-	
-	/*	int size = vect.size();
-		for(int i=1; i<size;++i){
-		for(int sorting =0; sorting<i+1;sorting++){
-			if(vect[i]< vect[sorting]){
-				swap(vector[sorting],vect[i]);
-			}
-		}
-	}
-}*/
-	
-}
-	
-	
-	/*	int *headTemp, *nextTemp;
-
-	for(headTemp = begin; headTemp!=end, ++headTemp){
-	
-		for(headTemp=begin;headTemp!=end, ++nextTemp){
+		for(nextTemp=headTemp+1;nextTemp!=end; ++nextTemp){
 			if(*(nextTemp)>*(nextTemp+1)){
 				swap(headTemp,(nextTemp+1));
 			}
@@ -63,4 +22,3 @@ void bubbleSort(int *begin, const int *end){
 		
 		
 }
-*/
